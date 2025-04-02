f772b943c9a1901426e45129995b795d34427cff
jkrusina <krusina.jos@gmail.com>
2025-04-02 17:59:05 +0200

    feat(accounts): make `Accounts` use VS/CA

--- apps/finance-manager/src/modules/accounts/accounts.module.ts
+++ apps/finance-manager/src/modules/accounts/accounts.module.ts
@@ -1,13 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AccountsController } from './accounts.controller';
-import { AccountsService } from './accounts.service';
 import { AuthModule } from '../auth/auth.module';
-import { AccountsRepository } from './repositories/accounts.repository';
+import { AccountsController } from './api/controllers/accounts.controller';
+import { AccountsRepository } from './infrastructure/repositories/accounts.repository';
+import { CreateAccountUseCase } from './application/create-account.use-case';
+import { RemoveAccountUseCase } from './application/remove-account.use-case';
+import { UpdateAccountUseCase } from './application/update-account-use-case';
+import { GetAccountBalanceUseCase } from './application/get-account-balance.use-case';
+import { FindOneAccountUseCase } from './application/find-one-account.use-case';
+import { GetTotalBalanceUseCase } from './application/get-total-balance.use-case';
+import { FindAllAccountsUseCase } from './application/find-all-accounts.use-case';
+
+const useCases = [
+  CreateAccountUseCase,
+  RemoveAccountUseCase,
+  UpdateAccountUseCase,
+  GetAccountBalanceUseCase,
+  FindOneAccountUseCase,
+  FindAllAccountsUseCase,
+  GetTotalBalanceUseCase,
+];
 
 @Module({
   imports: [AuthModule],
   controllers: [AccountsController],
-  providers: [AccountsService, AccountsRepository],
-  exports: [AccountsService],
+  providers: [...useCases, AccountsRepository],
+  exports: [...useCases],
 })
-export class AccountsModule {}
+export class AccountsModule { }
---
+++ apps/finance-manager/src/modules/accounts/application/find-all-accounts.use-case.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { Account } from '../core/entities/accounts.entity';
+import { AccountsRepository } from '../infrastructure/repositories/accounts.repository';
+
+@Injectable()
+export class FindAllAccountsUseCase {
+  constructor(private readonly accountsRepository: AccountsRepository) {}
+
+  async execute(userId: string): Promise<Account[]> {
+    return this.accountsRepository.findAll(userId);
+  }
+}
