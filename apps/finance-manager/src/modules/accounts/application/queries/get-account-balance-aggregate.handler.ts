32cea85f024c5e2a6136f6897c75f9520ed55cf6
jkrusina <krusina.jos@gmail.com>
2025-04-07 16:51:42 +0200

    feat(account): add `AggregateRoot` for `Accounts`

---
+++ apps/finance-manager/src/modules/accounts/application/queries/get-account-balance-aggregate.handler.ts
@@ -0,0 +1,36 @@
+import { IQuery, IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { AccountAggregateRepository } from '../../infrastructure/repositories/accounts-aggregate.repository';
+import { NotFoundException } from '@nestjs/common';
+
+export class GetAccountBalanceAggregateQuery implements IQuery {
+  constructor(
+    public readonly id: string,
+    public readonly userId: string,
+  ) {}
+}
+
+@QueryHandler(GetAccountBalanceAggregateQuery)
+export class GetAccountBalanceAggregateQueryHandler
+  implements IQueryHandler<GetAccountBalanceAggregateQuery>
+{
+  constructor(
+    private readonly accountAggregateRepository: AccountAggregateRepository,
+  ) {}
+
+  async execute(
+    query: GetAccountBalanceAggregateQuery,
+  ): Promise<{ balance: number }> {
+    const { id, userId } = query;
+
+    const accountAggregate = await this.accountAggregateRepository.findById(
+      id,
+      userId,
+    );
+
+    if (!accountAggregate) {
+      throw new NotFoundException(`Account with ID ${id} not found`);
+    }
+
+    return { balance: accountAggregate.initialBalance };
+  }
+}
