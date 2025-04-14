aa432108144e7ea47b03bf1859efb41ee036f8e8
jkrusina <krusina.jos@gmail.com>
2025-04-14 20:03:02 +0200

    chore: cleanup

--- apps/finance-manager/src/app.module.ts
+++ apps/finance-manager/src/app.module.ts
@@ -1,14 +1,29 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { APP_PIPE } from "@nestjs/core";
 import { ZodValidationPipe } from "nestjs-zod";
 import { AuthModule } from "./modules/auth/auth.module";
 import { UsersModule } from "./modules/users/users.module";
 import { HealthModule } from "./modules/health/health.module";
 import { AccountsModule } from "./modules/accounts/accounts.module";
-import { EnvModule } from "./modules/shared-kernel/infrastructure/env-config/env.module";
+import { appSchema } from "./modules/shared-kernel/infrastructure/env-config/env.schema";
 
 @Module({
-    imports: [EnvModule, AuthModule, UsersModule, HealthModule, AccountsModule],
+    imports: [
+        ConfigModule.forRoot({
+            validate: (config) => {
+                const result = appSchema.safeParse(config);
+                if (!result.success) {
+                    throw new Error(`Config validation error}`);
+                }
+                return result.data;
+            },
+        }),
+        AuthModule,
+        UsersModule,
+        HealthModule,
+        AccountsModule,
+    ],
     providers: [
         {
             provide: APP_PIPE,
