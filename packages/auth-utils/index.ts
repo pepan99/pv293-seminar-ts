6889547c25a03bd37ac124071d71fd3385366c1f
jkrusina <krusina.jos@gmail.com>
2025-04-15 13:02:57 +0200

    chore: remove stuff from packages

--- packages/auth-utils/index.ts
+++
@@ -1,6 +0,0 @@
-// Re-export all submodules
-export * from "./decorators/roles.decorator";
-export * from "./decorators/user.decorator";
-export * from "./guards/roles.guard";
-export * from "./guards/jwt.guard";
-export * from "./types/types";
