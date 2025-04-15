d802be6d873e3ba3bc1a70c127b651e26e6b9546
jkrusina <krusina.jos@gmail.com>
2025-04-15 16:27:26 +0200

    fix(config): all modules have separate configuration

--- apps/finance-manager/src/modules/accounts/accounts.module.ts
+++ apps/finance-manager/src/modules/accounts/accounts.module.ts
@@ -1,55 +1,54 @@
-import { Module } from "@nestjs/common";
-import { CqrsModule } from "@nestjs/cqrs";
-import { AccountsController } from "./api/controllers/accounts.controller";
-import { AccountsRepository } from "./infrastructure/database/repositories/accounts.repository";
-import { AccountAggregateRepository } from "./infrastructure/database/repositories/accounts-aggregate.repository";
+import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { AccountsController } from './api/controllers/accounts.controller';
+import { AccountsRepository } from './infrastructure/database/repositories/accounts.repository';
+import { AccountAggregateRepository } from './infrastructure/database/repositories/accounts-aggregate.repository';
 
-import { CreateAccountCommandHandler } from "./application/commands/create-account.handler";
-import { RemoveAccountCommandHandler } from "./application/commands/remove-account.handler";
-import { UpdateAccountCommandHandler } from "./application/commands/update-account.handler";
-import { GetAccountBalanceQueryHandler } from "./application/queries/get-account-balance.handler";
-import { GetAccountByIdQueryHandler } from "./application/queries/get-account-by-id.handler";
-import { GetTotalBalanceQueryHandler } from "./application/queries/get-total-balance.handler";
-import { GetAllAccountsQueryHandler } from "./application/queries/get-all-accounts.handler";
-import { ReconcileAccountCommandHandler } from "./application/commands/reconcile-account.handler";
-import { DatabaseModule } from "../shared-kernel/infrastructure/database/database.module";
-import { AccountConfigService } from "./infrastructure/config/account-config.service";
-import { AccountConfigModule } from "./infrastructure/config/account-config.module";
-import { ConfigService } from "@nestjs/config";
+import { CreateAccountCommandHandler } from './application/commands/create-account.handler';
+import { RemoveAccountCommandHandler } from './application/commands/remove-account.handler';
+import { UpdateAccountCommandHandler } from './application/commands/update-account.handler';
+import { GetAccountBalanceQueryHandler } from './application/queries/get-account-balance.handler';
+import { GetAccountByIdQueryHandler } from './application/queries/get-account-by-id.handler';
+import { GetTotalBalanceQueryHandler } from './application/queries/get-total-balance.handler';
+import { GetAllAccountsQueryHandler } from './application/queries/get-all-accounts.handler';
+import { ReconcileAccountCommandHandler } from './application/commands/reconcile-account.handler';
+import { AccountConfigService } from './infrastructure/config/account-config.service';
+import { AccountConfigModule } from './infrastructure/config/account-config.module';
+import { DatabaseModule } from '../shared-kernel/infrastructure/database/database.module';
 
 const commandHandlers = [
-    CreateAccountCommandHandler,
-    RemoveAccountCommandHandler,
-    UpdateAccountCommandHandler,
-    ReconcileAccountCommandHandler,
+  CreateAccountCommandHandler,
+  RemoveAccountCommandHandler,
+  UpdateAccountCommandHandler,
+  ReconcileAccountCommandHandler,
 ];
 
 const queryHandlers = [
-    GetAccountBalanceQueryHandler,
-    GetAccountByIdQueryHandler,
-    GetTotalBalanceQueryHandler,
-    GetAllAccountsQueryHandler,
+  GetAccountBalanceQueryHandler,
+  GetAccountByIdQueryHandler,
+  GetTotalBalanceQueryHandler,
+  GetAllAccountsQueryHandler,
 ];
 
 @Module({
-    imports: [
-        CqrsModule,
-        AccountConfigModule,
-        DatabaseModule.forFeatureAsync({
-            imports: [AccountConfigModule],
-            injects: [AccountConfigService],
-            // @ts-ignore
-            useFactory: (configService: AccountConfigService) => ({
-                host: configService.postgresHost,
-                port: configService.postgresPort,
-                user: configService.postgresUser,
-                password: configService.postgresPassword,
-                database: configService.postgresDB,
-            }),
-            inject: [AccountConfigService],
-        }),
-    ],
-    controllers: [AccountsController],
+  imports: [
+    CqrsModule,
+    AccountConfigModule,
+    DatabaseModule.forFeatureAsync({
+      imports: [AccountConfigModule],
+      injects: [AccountConfigService],
+      // @ts-ignore
+      useFactory: (configService: AccountConfigService) => ({
+        host: configService.postgresHost,
+        port: configService.postgresPort,
+        user: configService.postgresUser,
+        password: configService.postgresPassword,
+        database: configService.postgresDB,
+      }),
+      inject: [AccountConfigService],
+    }),
+  ],
+  controllers: [AccountsController],
   providers: [
     {
       provide: 'IAccountsRepository',
@@ -62,6 +61,6 @@ const queryHandlers = [
     ...commandHandlers,
     ...queryHandlers,
   ],
-    exports: [AccountsRepository, AccountAggregateRepository, CqrsModule],
+  exports: [AccountsRepository, AccountAggregateRepository, CqrsModule],
 })
 export class AccountsModule {}
--- apps/finance-manager/src/modules/users/users.module.ts
+++ apps/finance-manager/src/modules/users/users.module.ts
@@ -1,63 +1,66 @@
-import { Module } from "@nestjs/common";
-import { CqrsModule } from "@nestjs/cqrs";
-import { UsersController } from "./api/controllers/users.controller";
-import { CreateUserCommandHandler } from "./application/commands/create-user.handler";
-import { UpdateUserCommandHandler } from "./application/commands/update-user.handler";
-import { UpdateUserAdminCommandHandler } from "./application/commands/update-user-admin.handler";
-import { RemoveUserCommandHandler } from "./application/commands/remove-user.handler";
-import { ChangePasswordCommandHandler } from "./application/commands/change-password.handler";
-import { GetAllUsersQueryHandler } from "./application/queries/get-all-users.handler";
-import { GetUserByIdQueryHandler } from "./application/queries/get-user-by-id.handler";
-import { GetUserByEmailQueryHandler } from "./application/queries/get-user-by-email.handler";
-import { DatabaseModule } from "../shared-kernel/infrastructure/database/database.module";
-import { ConfigModule } from "@nestjs/config";
-import { UsersRepository } from "./infrastructure/database/repositories/users.repository";
-import { UserAggregateRepository } from "./infrastructure/database/repositories/users-aggregate.repository";
-import { DbEnv, dbSchema } from "../shared-kernel/infrastructure/env-config/env.schema";
-import { EnvModule } from "../shared-kernel/infrastructure/env-config/env.module";
-import { EnvService } from "../shared-kernel/infrastructure/env-config/env.service";
+import { Module } from '@nestjs/common';
+import { CqrsModule } from '@nestjs/cqrs';
+import { UsersController } from './api/controllers/users.controller';
+import { CreateUserCommandHandler } from './application/commands/create-user.handler';
+import { UpdateUserCommandHandler } from './application/commands/update-user.handler';
+import { UpdateUserAdminCommandHandler } from './application/commands/update-user-admin.handler';
+import { RemoveUserCommandHandler } from './application/commands/remove-user.handler';
+import { ChangePasswordCommandHandler } from './application/commands/change-password.handler';
+import { GetAllUsersQueryHandler } from './application/queries/get-all-users.handler';
+import { GetUserByIdQueryHandler } from './application/queries/get-user-by-id.handler';
+import { GetUserByEmailQueryHandler } from './application/queries/get-user-by-email.handler';
+import { DatabaseModule } from '../shared-kernel/infrastructure/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { UsersRepository } from './infrastructure/database/repositories/users.repository';
+import { UserAggregateRepository } from './infrastructure/database/repositories/users-aggregate.repository';
+import {
+  DbEnv,
+  dbSchema,
+} from '../shared-kernel/infrastructure/env-config/env.schema';
+import { EnvModule } from '../shared-kernel/infrastructure/env-config/env.module';
+import { EnvService } from '../shared-kernel/infrastructure/env-config/env.service';
 
 const commandHandlers = [
-    CreateUserCommandHandler,
-    UpdateUserCommandHandler,
-    UpdateUserAdminCommandHandler,
-    RemoveUserCommandHandler,
-    ChangePasswordCommandHandler,
+  CreateUserCommandHandler,
+  UpdateUserCommandHandler,
+  UpdateUserAdminCommandHandler,
+  RemoveUserCommandHandler,
+  ChangePasswordCommandHandler,
 ];
 
 const queryHandlers = [
-    GetUserByIdQueryHandler,
-    GetUserByEmailQueryHandler,
-    GetAllUsersQueryHandler,
+  GetUserByIdQueryHandler,
+  GetUserByEmailQueryHandler,
+  GetAllUsersQueryHandler,
 ];
 
 @Module({
-    imports: [
-        CqrsModule,
-        ConfigModule.forRoot({
-            envFilePath: ["./src/modules/users/.env"],
-            validate: (config) => {
-                const result = dbSchema.safeParse(config);
-                if (!result.success) {
-                    throw new Error(`Config validation error}`);
-                }
-                return result.data;
-            },
-        }),
-        EnvModule,
-        DatabaseModule.forFeatureAsync({
-            imports: [EnvModule],
-            inject: [EnvService],
-            useFactory: (envService: EnvService<DbEnv>) => ({
-                host: envService.get("POSTGRES_HOST"),
-                port: envService.get("POSTGRES_PORT"),
-                user: envService.get("POSTGRES_USER"),
-                password: envService.get("POSTGRES_PASSWORD"),
-                database: envService.get("POSTGRES_DB"),
-            }),
-        }),
-    ],
-    controllers: [UsersController],
+  imports: [
+    CqrsModule,
+    ConfigModule.forRoot({
+      envFilePath: ['./src/modules/users/.env'],
+      validate: (config) => {
+        const result = dbSchema.safeParse(config);
+        if (!result.success) {
+          throw new Error(`Config validation error}`);
+        }
+        return result.data;
+      },
+    }),
+    EnvModule,
+    DatabaseModule.forFeatureAsync({
+      imports: [EnvModule],
+      inject: [EnvService],
+      useFactory: (envService: EnvService<DbEnv>) => ({
+        host: envService.get('POSTGRES_HOST'),
+        port: envService.get('POSTGRES_PORT'),
+        user: envService.get('POSTGRES_USER'),
+        password: envService.get('POSTGRES_PASSWORD'),
+        database: envService.get('POSTGRES_DB'),
+      }),
+    }),
+  ],
+  controllers: [UsersController],
   providers: [
     {
       provide: 'IUsersRepository',
