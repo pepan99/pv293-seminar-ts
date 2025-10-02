ead8d9f136250566254d93bdf906674d21b56f14
Juan Marcos <juanmarcosu2@gmail.com>
2025-10-02 19:07:28 +0200

    feat: implement migrations

---
+++ apps/finance-manager/src/migrations/20250322185234_insert_admin.ts
@@ -0,0 +1,46 @@
+import {config} from 'dotenv';
+import * as bcrypt from 'bcrypt';
+import {ConfigService} from '@nestjs/config';
+import {Env} from '../modules/config/env';
+import {Database} from '../modules/database/database';
+import {EnvService} from '../modules/config/env.service';
+
+config();
+
+const configService = new ConfigService<Env, true>();
+const envService = new EnvService(configService);
+
+export async function up(db: Database) {
+  const name = envService.get('ADMIN_NAME');
+  const email = envService.get('ADMIN_EMAIL');
+  const password = envService.get('ADMIN_PASSWORD');
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const id = crypto.randomUUID();
+
+  await db
+    .insertInto('users')
+    .values({
+      id,
+      email,
+      password: hashedPassword,
+      name,
+    })
+    .execute();
+
+  await db
+    .insertInto('usersRoles')
+    .values({userId: id, role: 'admin'})
+    .execute();
+
+  await db
+    .insertInto('usersRoles')
+    .values({userId: id, role: 'user'})
+    .execute();
+}
+
+export async function down(db: Database) {
+  const email = envService.get('ADMIN_EMAIL');
+
+  await db.deleteFrom('users').where('email', '=', email).execute();
+}
