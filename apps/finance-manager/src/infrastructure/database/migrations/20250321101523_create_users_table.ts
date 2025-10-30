2d13e00a68704999b580bed4aa28d1d6fbf6c507
jkrusina <krusina.jos@gmail.com>
2025-10-30 18:27:18 +0100

    fix: some stuff

--- apps/finance-manager/src/infrastructure/database/migrations/20250321101523_create_users_table.ts
+++ apps/finance-manager/src/infrastructure/database/migrations/20250321101523_create_users_table.ts
@@ -1,7 +1,6 @@
 import { Kysely, sql } from 'kysely';
-import { DB } from '../common/types/db';
 
-export async function up(db: Kysely<DB>): Promise<void> {
+export async function up(db: Kysely<any>): Promise<void> {
   await db.schema.createType('user_role').asEnum(['admin', 'user']).execute();
 
   await db.schema
@@ -31,7 +30,7 @@ export async function up(db: Kysely<DB>): Promise<void> {
     .execute();
 }
 
-export async function down(db: Kysely<DB>): Promise<void> {
+export async function down(db: Kysely<any>): Promise<void> {
   await db.schema.dropIndex('users_email_idx').execute();
   await db.schema.dropTable('users').execute();
   await db.schema.dropTable('users_roles').execute();
